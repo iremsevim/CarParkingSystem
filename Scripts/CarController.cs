cbbf34413889f953f0032d84818748a5356ca7f9
iremsevim <iremsevim2442@outlook.com>
2020-08-24 12:01:31 +0300

    2DCarsUpload

---
+++ Scripts/CarController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{  
+    public float acceleration;
+    private Rigidbody2D rb;
+
+    public List<Collider2D> parkobjects;
+
+     void Start () 
+     {
+         rb = GetComponent<Rigidbody2D>();
+     }
+ 
+     void FixedUpdate ()
+    {
+         float updown= Input.GetAxis("Vertical");
+ 
+         Vector2 speed = transform.up * (updown * acceleration);
+         rb.AddForce(speed);
+
+         rb.velocity=new Vector2(Mathf.Clamp(rb.velocity.x,-2f,2f),Mathf.Clamp(rb.velocity.y,-2f,2f));
+     }
+}
---
+++ Scripts/parkpoint.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class parkpoint : MonoBehaviour
+{
+    public LayerMask carmask;
+    public LayerMask parkpointmask;
+
+    public SpriteRenderer spriteRenderer;
+    public Color32 parkcolor;
+    public Color32 normal;
+
+       void Start()
+    {
+        spriteRenderer=GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        parkcontrol();
+    }
+
+    private void parkcontrol()
+    {
+      Collider2D hittedcar=Physics2D.OverlapBox(transform.position,transform.localScale,0,carmask);
+      if(hittedcar==null)return;
+
+      CarController hittedcarcontroller=hittedcar.GetComponent<CarController>();
+      
+      Collider2D[] hittedparkpoints=Physics2D.OverlapBoxAll(transform.position,transform.localScale,0,parkpointmask);
+
+
+      bool parkisok=true;  
+
+      foreach(var item in hittedcarcontroller.parkobjects)
+      {
+          bool result=false;
+          foreach(var item2 in hittedparkpoints)
+          {
+              if(item==item2)
+              {
+                  result=true;
+                  break;
+              }
+          }
+          if(result==false)
+          {
+              parkisok=false;
+              break;
+          }
+      }   
+       
+       if(parkisok)
+       {
+           spriteRenderer.color=parkcolor;
+       }
+       else
+       {
+           spriteRenderer.color=normal;
+       }
+
+    }
+}
